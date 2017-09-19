d4e066b6a23304076ed7dee76fd92c9cc3ae65c9
xiaobu <1172504039@qq.com>
2017-09-19 19:43:17 +0800

    添加单元测试

--- src/main/java/com/gfdz/repository/GirlRepository.java
+++ src/main/java/com/gfdz/repository/GirlRepository.java
@@ -1,17 +1,24 @@
 package com.gfdz.repository;
 
 import com.gfdz.entity.GirlEntity;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
 /**
  * Created by Administrator on 2017/3/26.
  */
+@CacheConfig(cacheNames = "girls")
 public interface GirlRepository extends JpaRepository<GirlEntity,Integer> {
     //通过年龄查询
-    public List<GirlEntity> findByAge(Integer age);
-
-    public GirlEntity findByName(String name);
-    public GirlEntity findById(String id);
+     List<GirlEntity> findByAge(Integer age);
+    @Cacheable(key = "#p0")
+     GirlEntity findByName(String name);
+     GirlEntity findById(String id);
+    @CachePut(key = "#p0.name")
+    GirlEntity save(GirlEntity girl);
 }
